15747fea5edb839001cbbceec285f7183a303540
IanNoble-Visium <79815888+IanNoble-Visium@users.noreply.github.com>
2025-06-03 03:49:20 -0400

    Switch to default export for theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '../lib/theme'
+import theme from '../lib/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
